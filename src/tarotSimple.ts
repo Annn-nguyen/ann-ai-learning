ed3ce23f928b7d9b26c9cec7d8921552901e79a4
Ann-Nguyen <starpy259@gmail.com>
2025-02-27 08:26:04 +0700

    update

--- src/tarotSimple.ts
+++ src/tarotSimple.ts
@@ -18,12 +18,12 @@ import {z} from "zod";
         temperature: 0
     });
 
-    const defineDeckPrompt = "Based on the {question}, define the deck: positions (3-5 cards user can draw to get the answer), greeting (greet the user and explain the cards to draw) and call function getCardsDrawn";
+    const defineDeckPrompt = "Based on the {question}, define the spread: positions (3-5 cards user can draw to get the answer), greeting (greet the user and explain the cards to draw) and call function getCardsDrawn";
     const getCardsDrawn = "Generate card for each position in {positions}";
     const readCardsdPrompt = "Based on the {question} and {cardsDrawn}, provide the tarot reading for each card drawn";
 
     const DeckDefine = z.object({
-        positions: z.array(z.string()).describe("Array of positions for the tarot reading, based on the question of the user"),
+        positions: z.array(z.string()).describe("Array of positions for the tarot spread, based on the question of the user"),
         greeting: z.string().describe("Greeting message to the user, explaining the positions and cards to draw"),
     });
 
@@ -52,9 +52,9 @@ import {z} from "zod";
 
 
     //define node to define cards to draw based on question
-    const defineCards = async(state: typeOf OverallState.State) : Promise<Partial<typeOf OverallState.State>> => {
-        const prompt = defineCardsPrompt.replace("{question}", State.question);
-        const response = await llm.withStructuredOutput(CardDefined, { name : "positions"} ).invoke(prompt);
+    const defineCards = async(state: typeof OverallState.State) : Promise<Partial<typeof OverallState.State>> => {
+        const prompt = defineDeckPrompt.replace("{question}", state.question);
+        const response = await llm.withStructuredOutput(DeckDefine, { name : "positions"} ).invoke(prompt);
         console.log("Output of defineCards node",response);
         return { positions: response.positions};
     } ;
